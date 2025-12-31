9cb9b6ffce697a36e2be85dea09a61414f179c6b
Abhishek Jagtap <abhishek.jagtap2003@gmail.com>
2025-12-31 17:25:48 +0530

    Initial commit with frontend and backend folders

---
+++ url-shortener/src/main/java/org/url/shortener/service/UrlMappingService.java
@@ -0,0 +1,109 @@
+package org.url.shortener.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.url.shortener.dto.ClickEventDTO;
+import org.url.shortener.dto.UrlMappingDTO;
+import org.url.shortener.models.ClickEvent;
+import org.url.shortener.models.UrlMapping;
+import org.url.shortener.models.User;
+import org.url.shortener.repository.ClickEventRepository;
+import org.url.shortener.repository.UrlMappingRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UrlMappingService {
+
+    private final UrlMappingRepository urlMappingRepository;
+    private final ClickEventRepository clickEventRepository;
+
+    public UrlMappingDTO createShortUrl(String originalUrl, User user) {
+        String shortUrl = generateShortUrl();
+        UrlMapping urlMapping = new UrlMapping();
+        urlMapping.setOriginalUrl(originalUrl);
+        urlMapping.setShortUrl(shortUrl);
+        urlMapping.setUser(user);
+        urlMapping.setCreatedDate(LocalDateTime.now());
+        UrlMapping savedUrlMapping = urlMappingRepository.save(urlMapping);
+        return convertToDto(savedUrlMapping);
+    }
+
+    public List<UrlMappingDTO> getUrlsByUser(User user) {
+        return urlMappingRepository.findByUser(user)
+                .stream()
+                .map(this::convertToDto)
+                .toList();
+    }
+
+    private UrlMappingDTO convertToDto(UrlMapping urlMapping) {
+        UrlMappingDTO urlMappingDTO = new UrlMappingDTO();
+        urlMappingDTO.setId(urlMapping.getId());
+        urlMappingDTO.setOriginalUrl(urlMapping.getOriginalUrl());
+        urlMappingDTO.setShortUrl(urlMapping.getShortUrl());
+        urlMappingDTO.setClickCount(urlMapping.getClickCount());
+        urlMappingDTO.setCreatedAt(urlMapping.getCreatedDate());
+        urlMappingDTO.setUsername(urlMapping.getUser().getUsername());
+        return urlMappingDTO;
+    }
+
+    private String generateShortUrl() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        Random random = new Random();
+        StringBuilder shortUrl = new StringBuilder(8);
+
+        for (int i = 0; i < 8; i++) {
+            shortUrl.append(characters.charAt(random.nextInt(characters.length())));
+        }
+
+        return shortUrl.toString();
+    }
+
+    public List<ClickEventDTO> getClickEventsByDate(String shortUrl, LocalDateTime startDate, LocalDateTime endDate) {
+        UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        if (urlMapping != null) {
+            return clickEventRepository.findByUrlMappingAndClickDateBetween(urlMapping, startDate, endDate).stream()
+                    .collect(Collectors.groupingBy(click -> click.getClickDate().toLocalDate(), Collectors.counting()))
+                    .entrySet().stream()
+                    .map(entry -> {
+                        ClickEventDTO clickEventDTO = new ClickEventDTO();
+                        clickEventDTO.setClickDate(entry.getKey());
+                        clickEventDTO.setCount(entry.getValue());
+                        return clickEventDTO;
+                    })
+                    .collect(Collectors.toList());
+        }
+        return null;
+    }
+
+    public Map<LocalDate, Long> getTotalClicksByUserAndDate(User user, LocalDate startDate, LocalDate endDate) {
+        List<UrlMapping> urlMappings = urlMappingRepository.findByUser(user);
+        List<ClickEvent> clickEvents = clickEventRepository.findByUrlMappingInAndClickDateBetween
+                (urlMappings, startDate.atStartOfDay(), endDate.plusDays(1).atStartOfDay());
+        return clickEvents.stream()
+                .collect(Collectors.groupingBy(click -> click.getClickDate().toLocalDate(), Collectors.counting()));
+    }
+
+    public UrlMapping getOriginalUrl(String shortUrl) {
+        UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        if (urlMapping != null) {
+            urlMapping.setClickCount(urlMapping.getClickCount() + 1);
+            urlMappingRepository.save(urlMapping);
+
+            ClickEvent clickEvent = new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRepository.save(clickEvent);
+        }
+        return urlMapping;
+    }
+
+}
+
---
+++ url-shortener/src/main/java/org/url/shortener/service/UserService.java
@@ -0,0 +1,45 @@
+package org.url.shortener.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.url.shortener.dto.LoginRequest;
+import org.url.shortener.models.User;
+import org.url.shortener.repository.UserRepository;
+import org.url.shortener.security.jwt.JwtAuthenticationResponse;
+import org.url.shortener.security.jwt.JwtUtils;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public JwtAuthenticationResponse loginUser(LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        String jwt = jwtUtils.generateToken(userDetails);
+        return new JwtAuthenticationResponse(jwt);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("Username not found with username: " + username));
+    }
+}
